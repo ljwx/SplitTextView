4098f6e536d18005ca2a35a7c2299161d0e07f09
ljwx2 <1146699396@qq.com>
2022-12-08 16:30:19 +0800

    完成Demo演示

--- app/src/main/java/com/ljwx/view/MainActivity.kt
+++ app/src/main/java/com/ljwx/view/MainActivity.kt
@@ -3,14 +3,13 @@ package com.ljwx.view
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ljwx.view.SplitTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val stv = findViewById<SplitTextView>(R.id.stv)
+        val stv = findViewById<SplitTextView>(R.id.split)
         stv.setTextCenter("中间文字")
         stv.setColorLeft(Color.parseColor("#000000"))
 
--- app/src/main/java/com/ljwx/view/ThreeTextView.kt
+++ app/src/main/java/com/ljwx/view/ThreeTextView.kt
@@ -12,7 +12,6 @@ import android.text.Layout
 import android.text.StaticLayout
 import android.text.TextPaint
 import android.util.AttributeSet
-import android.util.Log
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
